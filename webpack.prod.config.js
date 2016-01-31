8ba7d4732c9b5dbacc6007326c4cd17cebcf1005
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-01-31 15:58:32 +0000

    Tidy up of config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -6,25 +6,26 @@ var config = require("./webpack.dev.config");
 
 config.entry = './dev/app.jsx';
 
-config.output.publicPath = path.join(__dirname, 'dist');
+config.output.path = path.join(__dirname, 'dist');
+delete config.output.publicPath;
 
 config.plugins = [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      '__DEV__': false,
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true,
-        warnings: false
-      },
-      sourceMap: false
-    }),
-    new ExtractTextPlugin('style.css', { allChunks: true })
-  ];
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.DefinePlugin({
+    '__DEV__': false,
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      screw_ie8: true,
+      warnings: false
+    },
+    sourceMap: false
+  }),
+  new ExtractTextPlugin('style.css', { allChunks: true })
+];
 
 delete config.debug;
 delete config.devtool;
